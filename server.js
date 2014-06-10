2b31b165284df5d3ee84d24130511dc58c47369f
epeicher <ese.rober@gmail.com>
2014-06-11 00:24:37 +0200

    Stylus and resources added and working

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str,path){
+	return stylus(str).set('filename',path);
+}
+
+app.set('views',__dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger());
+app.use(bodyParser());
+app.use(stylus.middleware(
+{
+	src: __dirname + '/public',
+	compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function (req, res) {
+	res.render('index.jade');
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
