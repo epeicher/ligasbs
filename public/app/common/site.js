4caeb84093d77d3cc94d6759d134bc60b9d117fd
epeicher <ese.rober@gmail.com>
2014-07-02 09:07:44 +0200

    Headroom used to hide the navigation bar. No animation

---
+++ public/app/common/site.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+	// grab an element
+	var myElement = document.querySelector("header");
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement);
+	// initialise
+	headroom.init(); 
+
+
+}());
