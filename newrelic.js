0272075336811ebd40507c7c5f45120fe480ab62
epeicher <ese.rober@gmail.com>
2014-11-11 21:47:01 +0100

    Newrelic added

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['ligasbs'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '0310404de84e59c47107e831c4507b08760515b9',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 var express = require('express');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
